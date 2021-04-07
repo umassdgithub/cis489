1a82d4773db5e8928697ae8e50ff2c7913c50796
DataVizUMassD <70420435+umassdgithub@users.noreply.github.com>
2021-04-06 23:06:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
+
 
 app.get('/',(req,res)=>{
-    res.send("Working fine");
+    fs.readFile("./test.txt",(err,data)=>{
+        res.send(`data: ${data}`);
+    })
+    
+    const data = req.query["q"]
+    fs.appendFile("./test.txt",data,(err)=>{
+        if(err){
+            console.log('err')
+        }
+        else{
+            console.log('data appended')
+        }
+    })
 })
-let port = process.env.PORT || 8080
+let port = process.env.PORT || 3000
 app.listen(port,()=>{console.log('Server running')})
