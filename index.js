03aaf2aca1c63579f46279192f9e2008fdaa93a1
DataVizUMassD <70420435+umassdgithub@users.noreply.github.com>
2021-04-07 10:29:51 -0400

    search

--- index.js
+++ index.js
@@ -5,23 +5,30 @@ const fs = require('fs');
 
 app.get('/',(req,res)=>{
     let data = req.query["q"]
+
     if(data===undefined){
-        data = "Not Received data from query"
+        data = `The query should contain key and value ?q=language`
+        res.status(400).send(data)    
     }
     else{
-        fs.readFile("./test.txt",(err,data)=>{
-            res.send(`data: ${data}`);
-        })
-        fs.appendFile("./test.txt",data,(err)=>{
+        fs.readFile("./Data/Some_Books.json",(err,booksData)=>{
+            booksData = JSON.parse(booksData);
             if(err){
-                console.log('err')
+                res.status(400).send('Data was not Accessable!') 
             }
+            const results = 
+            booksData.filter(d=>d.language
+                                .toLowerCase()
+                                .includes(data.toLowerCase().trim()))
+            if(results.length<=0){
+                res.status(400).send('No results found!') 
+            }   
             else{
-                console.log('data appended')
-            }
+                res.status(200).end(JSON.stringify(results))
+            }                             
+            
         })
     }
-
 })
 let port = process.env.PORT || 3000
 app.listen(port,()=>{console.log('Server running')})
